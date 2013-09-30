eb3e98a443a41f2911f67494ac534b4fccbd7349
Marcos Neves <marcos.neves@gmail.com>
2013-09-30 11:13:20 -0300

    adiciona suporte a parâmetro de linha de comando para o console.php, facilitando a utilizacao

--- console.php
+++ console.php
@@ -1,11 +1,14 @@
 <?php
 
 // Utilize este arquivo para testar rapidamente a classe pela linha de comando
+// executar assim: php console.php 123.45
 
 require_once 'Troco.php';
 
+$reais = $argv[1] ?: 289.99;
+
 $troco = new Troco();
 
-$notas = $troco->getQtdeNotas(289.99);
+$notas = $troco->getQtdeNotas($reais);
 
 print_r($notas);
