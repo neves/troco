12888f609d82945f42016419005551c3cfa88cfe
Daniel Fernando Lourusso <dflourusso@gmail.com>
2013-10-02 00:29:57 -0300

    Tarefa concluída.

--- Troco.php
+++ Troco.php
@@ -7,34 +7,81 @@
  */
 class Troco
 {
-    /**
-     * Dado um valor em reais, retorna a quantidade de notas necessÃ¡rias para formar o troco.
-     *
-     * @param $reais Valor em reais, podendo conter centavos.
-     * @return array Quantidade de notas, indexada pelo seu valor.
-     */
+     //Array com as notas Disponiveis
+     var $Disp = array( "100" => INF,
+                            "50" => INF,
+                            "20" => INF,
+                            "10" => INF,
+                            "5" => INF,
+                            "2" => INF,
+                            "1" => INF,
+                            "0.5" => INF,
+                            "0.25" => INF,
+                            "0.1" => INF,
+                            "0.01" => INF);
+    /*
+    * Dado um valor em reais, retorna a quantidade de notas necessÃ¡rias para formar o troco.
+    *
+    * @param $reais Valor em reais, podendo conter centavos.
+    * @return array Quantidade de notas, indexada pelo seu valor.
+    */
     public function getQtdeNotas($reais)
-    {
-        $qtdeNotas = [
-          "100" => 0,
-           "50" => 0,
-           "20" => 0,
-           "10" => 0,
-            "5" => 0,
-            "2" => 0,
-            "1" => 0,
-          "0.5" => 0,
-         "0.25" => 0,
-          "0.1" => 0,
-         "0.01" => 0,
-        ];
+    {   $value;
+        $key;
+        $nDisp = $this->Disp;
+        $valorTotal = 0;
+        $qtdeNotas = array( "100" => 0,
+                            "50" => 0,
+                            "20" => 0,
+                            "10" => 0,
+                            "5" => 0,
+                            "2" => 0,
+                            "1" => 0,
+                            "0.5" => 0,
+                            "0.25" => 0,
+                            "0.1" => 0,
+                            "0.01" => 0);
 
-        // colocar o cÃ³digo aqui, exemplo:
-
-        if ($reais == 100) {
-            $qtdeNotas["100"] = 1;
-        }
-
-        return $qtdeNotas;
+        /*
+        * Foreach + While responsaveis por incrementar as notas
+        * ate que a quantidade chegue no valor desejado.
+        */
+        try
+        {
+           foreach ($qtdeNotas as $key => $value)
+           {
+               while ($valorTotal < $reais)
+               {
+                    if ($valorTotal + $key <= $reais && $nDisp[$key] > 0)
+                    {
+                        $valorTotal += $key;
+                        $qtdeNotas[$key] ++;
+                        $nDisp[$key] --;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+          
+            /*
+            * Se após a utilização de todas as notas disponíveis o valor ainda não
+            * for alcançado dá o erro.
+            */
+            If($valorTotal < $reais)
+            {
+                // Cria o erro.
+                throw new RuntimeException("<b>Atenção: Notas insuficientes</b>");
+            }
+          }
+          catch (RuntimeException $rex)
+          {
+              // Apresenta a mesagem de erro e sai da função.
+              die($rex->getMessage());
+          }
+          return $qtdeNotas;
     }
 }
+?>
+
--- console.php
+++ console.php
@@ -1,14 +1,29 @@
 <?php
 
 // Utilize este arquivo para testar rapidamente a classe pela linha de comando
-// executar assim: php console.php 123.45
 
 require_once 'Troco.php';
 
-$reais = $argv[1] ?: 289.99;
-
 $troco = new Troco();
 
-$notas = $troco->getQtdeNotas($reais);
+/*
+ * Abaixo segue a variável com as notas necessárias.
+ * Se for informada a classe utiliza para fazer o cálculo,
+ * caso contrário ela utiliza um array infinito.
+$troco->Disp = array( "100" => 3,
+                       "50" => 0,
+                       "20" => 0,
+                       "10" => 1,
+                        "5" => 0,
+                        "2" => 0,
+                        "1" => 0,
+                      "0.5" => 0,
+                     "0.25" => 0,
+                      "0.1" => 0,
+                     "0.01" => 0);
+*/
+                     
+$notas = $troco->getQtdeNotas(289.99);
 
 print_r($notas);
+?>
